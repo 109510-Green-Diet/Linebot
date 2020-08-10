88285009fb98fb95d32c63a0ec8749841a962001
unknown <eaet10556ntub@gmail.com>
2020-08-10 12:35:02 +0800

    myApp

--- index.js
+++ index.js
@@ -38,10 +38,10 @@ app.post('/dialogflow', express.json(), (request, response) => {
         agent.add('歡迎你!!');
 
         //取得會員的LineID
-        var id = request.body.originalDetectIntentRequest.payload.data.source.userId;
+        var user_account = request.body.originalDetectIntentRequest.payload.data.source.userId;
 
         //呼叫customer模組, 寫入會員資料
-        return customer.add(id).then(data => {  
+        return customer.add(user_account).then(data => {  
             if (data == -9){
                 //回覆文字
                 agent.add('喔, 你的會員原本就存在!');
