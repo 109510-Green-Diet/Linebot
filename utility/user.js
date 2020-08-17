346ff8ba5a6a1ea9e439ae0c3240407a0382be57
unknown <eat10556ntub@gmail.com>
2020-08-17 12:12:40 +0800

    myApp

--- utility/user.js
+++ utility/user.js
@@ -6,12 +6,12 @@ const query = require('./asyncDB');
 //---------------------------------
 // 加入會員 (寫id到user資料表)
 //---------------------------------
-var add = async function(user_account){
+var add = async function(newData){
     //存放結果
-    let result;  
+    var result;  
 
     //寫入資料表
-    await query('INSERT INTO project.user (user_account) VALUES ($1) ', [newData.user_account])
+    await sql('INSERT INTO project.user (user_account) VALUES ($1) ', [newData.user_account])
         .then((data) => {
             result = 0;   //寫入成功
         }, (error) => {
