01dc3ecd37859937a3b519dbe4e01ce3d996833f
10556042 <eat10556ntub@gmail.com>
2020-09-06 22:50:33 +0800

    myApp

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ app.post('/dialogflow', express.json(), (request, response) => {
         agent.add('查看的食譜是:' + recipe_name);
 
         //呼叫menu模組, 取出分類菜單
-        return project.recipe.showrecipe(recipe_name).then(data => {
+        return recipe.showrecipe(recipe_name).then(data => {
             if (data == -9) {
                 //回覆文字            
                 agent.add('喔, 讀取資料錯誤(程式或資料庫出錯)!');
