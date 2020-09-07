612597e3ccb23f449b8264beef3ce6369d3041f1
unknown <eat10556ntub@gmail.com>
2020-09-07 11:09:40 +0800

    myApp

--- utility/recipe.js
+++ utility/recipe.js
@@ -11,9 +11,11 @@ var showrecipe = async function(recipe_name){
     //存放結果
     let result;  
     recipe_name = "%" + recipe_name + "%";
+    console.log(recipe_name);
     //讀取資料庫
     await query('SELECT * FROM project.recipe WHERE recipe_name like $1 order by random() limit 10', [recipe_name])
         .then((data) => {
+            console.log(data);
             result = data.rows;   //查詢成功
         }, (error) => {
             result = -9;          //查詢失敗
