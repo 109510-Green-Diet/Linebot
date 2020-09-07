5e4d4cc1ee8623ef1fefed3acad7c61790f7b469
unknown <eat10556ntub@gmail.com>
2020-09-07 10:56:09 +0800

    myApp

--- utility/recipe.js
+++ utility/recipe.js
@@ -12,7 +12,7 @@ var showrecipe = async function(recipe_name){
     let result;  
     recipe_name = "%" + recipe_name + "%";
     //讀取資料庫
-    await query('SELECT * FROM recipe WHERE recipe_name like $1 order by random() limit 10', [recipe_name])
+    await query('SELECT * FROM project.recipe WHERE recipe_name like $1 order by random() limit 10', [recipe_name])
         .then((data) => {
             result = data.rows;   //查詢成功
         }, (error) => {
