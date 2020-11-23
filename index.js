a56d674c9c9467bf8ed404f3427343a76a37f514
10556026 <10556026@ntub.edu.tw>
2020-11-23 15:24:06 +0800

    myApp

--- index.js
+++ index.js
@@ -233,7 +233,7 @@ app.post('/dialogflow', express.json(), (request, response) => {
 
                 var payload = new Payload('LINE', lineMessage, { sendAsMessage: true });
                 agent.add(payload);
-            } else if (data == 0) {
+            } else if (data.length == 0) {
                 //回覆文字              
                 agent.add('喔, 目前沒有內容!');
 
