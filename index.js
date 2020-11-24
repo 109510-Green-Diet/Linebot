5899ce4fdc3909094c8ae16bc8d6e67d061b8c4a
10556042 <10556042@ntub.edu.tw>
2020-11-24 15:19:43 +0800

    myApp

--- index.js
+++ index.js
@@ -154,6 +154,12 @@ app.post('/dialogflow', express.json(), (request, response) => {
                             "label": "查看食譜",
                             "text": "查看" + data[i].recipe_name + "完整食譜"
 
+                        }],
+                        "actions": [{
+                            "type": "message",
+                            "label": "查看食譜",
+                            "text": "查看" + data[i].recipe_name + "完整食譜"
+
                         }]
                     })
                 }
